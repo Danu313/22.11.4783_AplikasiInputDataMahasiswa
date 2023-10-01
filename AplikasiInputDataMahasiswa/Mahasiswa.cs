5160111129d823a8d21d25bb7c8468e4a45907d3
wildanu <muwildanuus@students.amikom.ac.id>
2023-10-01 23:10:37 +0700

    Add project files.

---
+++ AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiInputDataMahasiswa
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string Kelas { get; set; }
+
+        public string Nhuruf;
+        private int nilai;
+        public int Nilai
+        {
+            get { return nilai; }
+            set
+            {
+                int val = value;
+                nilai = val;
+                if (val >= 81)
+                {
+                    Nhuruf = "A";
+                }
+                else if (val >= 61)
+                {
+                    Nhuruf = "B";
+                }
+                else if (val >= 41)
+                {
+                    Nhuruf = "C";
+                }
+                else if (val >= 21)
+                {
+                    Nhuruf = "D";
+                }
+                else
+                {
+                    Nhuruf = "E";
+                }
+            }
+        }
+    }
+}
